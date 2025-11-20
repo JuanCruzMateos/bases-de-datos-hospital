ffb83ae01d079fde4f7914850fdf62df55db6eed
Gianfranco <Giansanpedro@outlook.com>
2025-11-20 12:27:39 -0300

    Seteo inicial SP y Triggers

---
+++ db_scripts/init/10-rebuild-hospital.sql
@@ -0,0 +1,17 @@
+
+-- Este es el archivo maestro que carga todos los procedimientos almacenados y scripts necesarios
+-- para reconstruir la base de datos del esquema 'hospital'.
+
+ALTER SESSION SET CONTAINER = FREEPDB1;
+ALTER SESSION SET CURRENT_SCHEMA = hospital;
+
+@/opt/oracle/scripts/startup/01-drop-tables.sql
+@/opt/oracle/scripts/startup/02-create-tables-pk.sql
+@/opt/oracle/scripts/startup/03-define-fk-constrains.sql
+@/opt/oracle/scripts/startup/04-init-db.sql
+@/opt/oracle/scripts/startup/05-triggers.sql
+
+@/opt/oracle/scripts/procedures/sp_internaciones.sql
+@/opt/oracle/scripts/procedures/sp_camas_disponibles.sql
+@/opt/oracle/scripts/procedures/sp_comentarios_visitas.sql
+@/opt/oracle/scripts/procedures/sp_auditoria_guardias.sql
